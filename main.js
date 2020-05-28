a01a256f95847ef6fdb0a3fe042151367aaf7f9f
Cauany Souza <souza.cauany99@gmail.com>
2020-05-27 23:31:25 -0300

    exercicio 1 feito

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-const usuario = {
-    nome: 'greg',
-    idade: 25,
-    jogos: {
-        volei: 'volei de areia',
-        agua: 'natação',
-    },
-}
-const { idade, jogos: { volei }, nome } = usuario
+//ATIVIDADE !
+// class Usuario {
+//     constructor(email,senha){
+//         this.email = email
+//         this.senha = senha
+//     }
+    
+// }
 
-console.log(volei, idade)
+// class Admin extends Usuario {
+//     super(){
+
+//     }
+// }
+
+// const user1 = new Usuario('email@teste.com', 'senha345')
+// const admin1 = new Admin('email@teste.com', 'senha3489')
+
+// console.log(user1.email)
+// console.log(admin1)
+
+//ATIVIDADE 2
+
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+]
+
+const old = usuarios.map(
+    function(){
+        const oldUser = []
+
+        oldUser.push[usuarios.idade]
+    }
+)
+
+console.log(old)
+
